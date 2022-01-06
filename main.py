becd8e0facb2b187210b825d1d1299eaa3f383a7
user <cat15434@gmail.com>
2022-01-05 22:57:56 -0500

    end my suffering

---
+++ main.py
@@ -0,0 +1,13 @@
+print("how many chocolates has aunt alana ate this week!")
+print("please enter the amount of chocalates for each day if 0 enter 0")
+
+a = int(input("Sunday:"))
+b = int(input("Monday:"))
+c = int(input("Tuesday:"))
+d = int(input("Wednesday:"))
+e = int(input("Thursday:"))
+e = int(input("Friday:"))
+f = int(input("Saturday:"))
+
+tot=(a+b+c+d+e+f)
+print("Aunt Alana has eaten:", tot)
